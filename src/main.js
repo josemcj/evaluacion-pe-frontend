869fe79a9a786b9ebe62559d857ed11eac6a3f7d
Jose Contreras <mexidios.mx@gmail.com>
2026-07-16 01:59:24 -0600

    implementacion/instala y configura pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
+
 app.mount('#app');
