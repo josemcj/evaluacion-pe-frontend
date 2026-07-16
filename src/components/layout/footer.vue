00346685776001a15377edaafeb866943d0d3612
Jose Contreras <mexidios.mx@gmail.com>
2026-07-16 12:51:07 -0600

    implementacion/layout general

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="border-top bg-white px-4 py-3">
+    <div class="text-center text-muted">&copy; {{ new Date().getFullYear() }} App de transacciones.</div>
+  </footer>
+</template>
---
+++ src/components/layout/layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './header.vue';
+import Footer from './footer.vue';
+</script>
+
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    <Header />
+    <main class="container my-3 flex-grow-1">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
