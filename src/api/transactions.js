ecefd0c5106e2e6ee06afaf6233325ae09364336
Jose Contreras <mexidios.mx@gmail.com>
2026-07-16 15:34:01 -0600

    implementacion/agrega AES y Sweet Alert

--- src/api/transactions.js
+++ src/api/transactions.js
@@ -4,3 +4,8 @@ export const getTransactions = async () => {
   const response = await api.get('/transactions');
   return response.data;
 };
+
+export const createTransaction = async (transactionData) => {
+  const response = await api.post('/transactions/create', transactionData);
+  return response.data;
+};
--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 
+import { LoadingPlugin } from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(LoadingPlugin, { color: '#010f60' });
+app.use(VueSweetalert2);
 
 app.mount('#app');
