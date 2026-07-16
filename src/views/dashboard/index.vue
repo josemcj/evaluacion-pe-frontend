601a61c1c7caaa4a85d578b09a0f19f1d421bdcf
Jose Contreras <mexidios.mx@gmail.com>
2026-07-16 12:51:35 -0600

    implementacion/mostrar componente de acuerdo a rol

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,22 @@
 <script setup>
-import Header from '@/components/header.vue';
+import { computed } from 'vue';
+import Layout from '@/components/layout/layout.vue';
+import OperatorDashboard from './operator.vue';
+import SupervisorDashboard from './supervisor.vue';
+import { useAuthStore } from '@/stores/auth.js';
+
+const authStore = useAuthStore();
+
+const componentsByRole = {
+  Operador: OperatorDashboard,
+  Supervisor: SupervisorDashboard,
+};
+
+const DashboardComponent = computed(() => componentsByRole[authStore.role] || null);
 </script>
 
 <template>
-  <Header />
+  <Layout>
+    <component :is="DashboardComponent" />
+  </Layout>
 </template>
